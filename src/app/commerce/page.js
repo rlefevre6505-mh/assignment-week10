801ae6feb2068e06ac3f14922a41ce573fc58ca9
AMY LOUISE TUCK <amyltuck@hotmail.com>
2026-02-03 11:59:49 +0000

    feat: implement for loop to iterate 50 products

--- src/app/commerce/page.js
+++ src/app/commerce/page.js
@@ -1,41 +1,35 @@
 import { faker } from "@faker-js/faker";
-import Image from "next/image";
-import ProductGrid from "../components/ProductGrid";
+// import Image from "next/image";
 
-export default function CommerceTest() {
-  const department = faker.commerce.department();
-  const product = faker.commerce.productName();
-  const prodDescription = faker.commerce.productDescription();
-  const prodPrice = faker.commerce.price();
-  const plainImg = faker.image.dataUri();
-  const avatar = faker.image.avatar();
-  return (
-    <main>
-      <h1>{department}</h1>
-      <h2>{product}</h2>
-      <h3>{prodDescription}</h3>
-      <h4>£{prodPrice}</h4>
-      <Image
-        className="plain-img"
-        src={plainImg}
-        alt=""
-        width={300}
-        height={400}
-      />
-      <Image src={avatar} alt="" width={300} height={400} />
-    </main>
-  );
+export default function CommerceDemoPage() {
+  const prodArray = [];
+  async function generateArray() {
+    for (let i = 0; i < 50; i++) {
+      prodArray.push({
+        id: faker.string.uuid(),
+        name: faker.commerce.productName(),
+        description: faker.commerce.productDescription(),
+        price: faker.commerce.price({ min: 10, max: 100 }),
+        category: faker.commerce.department(),
+        image: faker.image.urlPicsumPhotos({ width: 300, height: 300 }),
+        rating: faker.number.int({ min: 1, max: 5 }),
+        inStock: faker.datatype.boolean(),
+      });
+    }
+    return prodArray;
+  }
+  generateArray();
+  console.log(prodArray);
 }
-
-export function generateProducts() {
-  return Array({
-    id: faker.string.uuid(),
-    name: faker.commerce.productName(),
-    description: faker.commerce.productDescription(),
-    price: faker.commerce.price({ min: 10, max: 100 }),
-    category: faker.commerce.department(),
-    image: faker.image.urlPicsumPhotos({ width: 300, height: 300 }),
-    rating: faker.number.float({ min: 1, max: 5, precision: 0.1 }),
-    inStock: faker.datatype.boolean(),
-  });
-}
+// <main>
+//   <h1>{products.name}</h1>
+//   <h2>£{products.price}</h2>
+//   <h3>{products.description}</h3>
+//   <h4>{products.rating}</h4>
+//   <Image
+//     src={products.image}
+//     alt={products.name}
+//     width={300}
+//     height={300}
+//   />
+// </main>
