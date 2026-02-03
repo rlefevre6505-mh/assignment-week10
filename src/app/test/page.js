25dd41031aeedfb273ee5642db173d87e63b6d2f
Richard Lefevre <rlefevre.6505@gmail.com>
2026-02-03 09:16:37 +0000

    commit

---
+++ src/app/test/page.js
@@ -0,0 +1,14 @@
+// import { Faker, es } from "@faker-js/faker";
+
+export default function TestPage() {
+  // const { Faker, es } = require("@faker-js/faker");
+  // const fakerES = new Faker({ locale: [es] });
+
+  return (
+    <>
+      <h1>Faker test</h1>
+      {/* <p>{fakerES.person.firstName()}</p>
+      <p>{fakerES.person.lastName()}</p> */}
+    </>
+  );
+}
