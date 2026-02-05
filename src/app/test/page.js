6c0f4a652409f18ff0424649d28a4e0ed8ec87cf
Richard Lefevre <rlefevre.6505@gmail.com>
2026-02-05 09:32:19 +0000

    fix: changed test page

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,18 +1,10 @@
-import { Faker, es } from "@faker-js/faker";
-import { db } from "@/utils/dbconnection";
+import { faker } from "@faker-js/faker";
 
 export default async function TestPage() {
-  // const { Faker, es } = require("@faker-js/faker");
-  const fakerES = new Faker({ locale: [es] });
-
-  const query = await db.query("SELECT * FROM blog_comments");
-  console.log(query);
-
   return (
     <>
-      <h1>Faker test</h1>
-      <p>{fakerES.person.firstName()}</p>
-      <p>{fakerES.person.lastName()}</p>
+      <h1>Faker Test</h1>
+      <p>{faker.person.fullName()}</p>
     </>
   );
 }
